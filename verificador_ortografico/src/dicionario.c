aaea16d4bbe1209ec5e97cf45abe59c25e12ff14
LucasMull <muller.lucas@Hotmail.com>
2020-08-13 16:48:31 -0300

    refactor: removi contagem de linhas do arquivo para realizar um único malloc, e coloquei um realloc que específica o tamanho da array no mesmo loop que faz a leitura das palavras

--- verificador_ortografico/src/dicionario.c
+++ verificador_ortografico/src/dicionario.c
@@ -6,34 +6,35 @@
 
 #include "dicionario.h"
 
+#define FILESIZE_COEFICIENT 131072
+
+/* extrai informações do arquivo dicionário em um array de strings,
+  com cada índice representando uma palavra e linha do dicionário */
 static void
 parse_dict(dict_t *dict, FILE *f_dict)
 {
-  dict->wcount=0;
-
-  int c;
-  while ((c=fgetc(f_dict)) != EOF){ //lê até fim do arqv
-    if (c=='\n') //conta uma linha
-      ++dict->wcount;
-  } 
-  assert(feof(f_dict)); //checa se leu arq corretamente
-  fseek(f_dict,0,SEEK_SET); //volta pro começo do f_dict
-
-  dict->wlist=malloc(dict->wcount*sizeof(char*));
-  assert(dict->wlist); //verifica mem alocada corretamente 
+  long memblock=0; //conta qtd de blocos criados na memória
+  char word[MAX_WORD_SIZE]; //armazena palavra por linha
+  dict->wlist = NULL;
 
   /*lê arq linha por linha, cada linha uma palavra, e
     armazena as palavras em dict->wlist*/
-  long i=0;
-  char word[MAX_WORD_SIZE]; //armazena palavra por linha
+  dict->wcount = 0;
   while (fgets(word, MAX_WORD_SIZE-1, f_dict) != NULL){
+    /*se qtd de linhas for multiplo do coeficiente especificado
+      então aloca espaço na memória para prosseguir com leitura
+      do arquivo*/
+    if (dict->wcount % FILESIZE_COEFICIENT == 0){
+      ++memblock;
+      dict->wlist = realloc(dict->wlist,memblock*FILESIZE_COEFICIENT*sizeof(char*));
+      assert(dict->wlist);
+    }
     //aloca mem para palavra sem incluir newline
-    dict->wlist[i] = strndup(word,strlen(word)-1);
-    ++i;
+    dict->wlist[dict->wcount] = strndup(word,strlen(word)-1);
+    assert(dict->wlist[dict->wcount]);
+
+    ++dict->wcount;
   }
-  //checa se contagem de linhas inicial é igual contagem de palavras
-  assert(i == dict->wcount);
-  //checa se terminou a leitura corretamente
   assert(feof(f_dict));
 }
 
@@ -54,11 +55,11 @@ set_dict(char dict_lang[], dict_t *dict)
   strcat(dict_location,dict_lang);
 
   FILE *f_dict=fopen(dict_location, "rb");
-  assert(f_dict); //checa se arq foi lido (ou existe);
+  assert(f_dict);
 
-  parse_dict(dict, f_dict); //configura var dicionario
+  parse_dict(dict, f_dict); //set membros do struct dict
 
-  //ordena palavras do dicionario para bsearch*/
+  //ordena palavras do dicionario para bsearch
   qsort(dict->wlist,dict->wcount,sizeof(char*),cstring_cmp);
 
   fclose(f_dict);
@@ -101,22 +102,31 @@ void
 mispelling_mark(const dict_t *dict, FILE* out_stream)
 {
   int c; //leitura de chars do stdin
-  char word[MAX_WORD_SIZE]; //forma palavra a ser pesquisada
+  char word[MAX_WORD_SIZE]; //formação da palavra a ser verificada
 
+  /* c lê o stdin um char por vez, com a condição de saída de chegar
+  no fim do arquivo (EOF).
+  enquanto c for um char alfabético, a palavra é formada na variável
+  word, quando não for char alfabético, a palavra formada é então
+  analisada para ver se existe ou não no dicionário*/
   int i=0;
-  while ((c=getchar()) != EOF){ //recolhe char até fim do arq 
-    if (isalpha(c)){ //se char for alpha
-      word[i] = c; //forma palavra
-      ++i; //e incremente posição para proximo char
-    } else { //char não é alpha
+  while ((c=getchar()) != EOF){
+    //se c for alfabético então concatena no final da palvra
+    //senão verifica existência da palavra formada no dicionário
+    if (isalpha(c)){
+      word[i] = c;
+      ++i;
+    } else {
       word[i] = '\0'; //concluir palavra formada
-      if ((i==0) || (dict_bsearch(dict,word) == 0)){ //se palavra n for alpha, ou existir no dict
-        fputs(word,out_stream); //imprime palavra s/colchetes
-      } else { //se não estiver no dict
-        fprintf(out_stream,"[%s]",word); //imprime com colchetes
+      //se palavra n for alpha, ou existir no dict imprime sem colchetes
+      //senão imprime palavra com colchetes
+      if ((i == 0) || (dict_bsearch(dict,word) == 0)){ 
+        fputs(word,out_stream);
+      } else {
+        fprintf(out_stream,"[%s]",word);
       }
-      i = 0; //reseta índice para formar nova palavra
 
+      i = 0; //reseta índice para formar nova palavra
       fputc(c,out_stream);//printa char não alpha encontrado
     }
   }
